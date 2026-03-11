6e2c8a3fa045f41b6ffbf908093097d109348fb4
Viniciuss2004 <viandcustodio@gmail.com>
2026-03-10 21:51:48 -0300

    Estrutura base

---
+++ src/main/java/br/com/reservacampo/GerenciamentoCampoApplication.java
@@ -0,0 +1,13 @@
+package br.com.reservacampo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoCampoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoCampoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/reservacampo/GerenciamentoCampoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.reservacampo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoCampoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
