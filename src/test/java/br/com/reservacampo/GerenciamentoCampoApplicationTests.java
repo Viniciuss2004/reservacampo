dc8a00ecb7ca363a65aab29c3a5053a46ff1f5ae
Ian Pires <ianbr23@gmail.com>
2026-03-25 21:02:10 -0300

    ativa o profile 'test' na classe principal de testes

--- src/test/java/br/com/reservacampo/GerenciamentoCampoApplicationTests.java
+++ src/test/java/br/com/reservacampo/GerenciamentoCampoApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.reservacampo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GerenciamentoCampoApplicationTests {
 
 	@Test
